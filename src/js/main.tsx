b4917201c6e29c5b92fd338c315cf7e21380b545
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-21 03:38:14 +0000

    feat: init [#1]

---
+++ src/js/main.tsx
@@ -0,0 +1,7 @@
+/**
+ * @description Main
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import {createRoot} from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(<h1>HELLO</h1>)
