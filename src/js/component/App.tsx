f6c4126bd1c28286f0f80155335df4a03d4ea58b
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-23 17:50:37 +0000

    feat: add float and mouse effects to laptop [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -2,7 +2,7 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import {Environment} from '@react-three/drei'
+import {Environment, Float, PresentationControls} from '@react-three/drei'
 import {Canvas} from '@react-three/fiber'
 import {Leva} from 'leva'
 import {Laptop} from './app/Laptop.tsx'
@@ -11,8 +11,17 @@ export const App = () => {
   return (
     <>
       <Canvas camera={{fov: 45, position: [1, 1, 5]}}>
-        <Environment preset="warehouse" />
-        <Laptop />
+        <Environment preset="apartment" environmentIntensity={0.8} />
+        <PresentationControls
+          global={true}
+          snap={true}
+          polar={[-Math.PI / 16, Math.PI / 16]}
+          azimuth={[-Math.PI / 16, Math.PI / 16]}
+        >
+          <Float>
+            <Laptop position-y={-1} />
+          </Float>
+        </PresentationControls>
       </Canvas>
       <Leva collapsed={true} />
     </>
