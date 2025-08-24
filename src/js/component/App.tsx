e14aaf324e2da0ff10350e7ba45c694e3e5cce14
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-24 06:52:48 +0000

    feat: add contact shadow [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -2,16 +2,44 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import {Environment, Float, PresentationControls} from '@react-three/drei'
+import {
+  ContactShadows,
+  Environment,
+  Float,
+  PresentationControls,
+} from '@react-three/drei'
 import {Canvas} from '@react-three/fiber'
-import {Leva} from 'leva'
+import {Leva, useControls} from 'leva'
 import {Laptop} from './app/Laptop.tsx'
 
 export const App = () => {
+  const {color, opacity, blur} = useControls('shadow', {
+    color: '#000000',
+    opacity: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+    },
+    blur: {
+      value: 5,
+      min: 0,
+      max: 20,
+    },
+  })
+
   return (
     <>
       <Canvas camera={{fov: 45, position: [1, 1, 5]}}>
         <Environment preset="apartment" environmentIntensity={0.8} />
+
+        <ContactShadows
+          position={[0, -1, 0]}
+          color={color}
+          opacity={opacity}
+          blur={blur}
+          frames={1}
+        />
+
         <PresentationControls
           global={true}
           snap={true}
